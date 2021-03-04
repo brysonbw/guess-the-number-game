8f6a012c1e5c5262ea35c1d0203e3a00fa1dd0e2
Bryson Ward <brysonward77@gmail.com>
2021-03-04 09:02:05 -0500

    Fun game. Player has 7 trys 2 guess random num

---
+++ guess_the_number.py
@@ -0,0 +1,22 @@
+# This is a guess the number game.
+
+import random
+secretNumber = random.randint(1,25)
+print("I am think of a number 1 and 25.")
+
+# Ask the player to guess 7 times
+for guessesTaken in range(1,8):
+    print("Take a guess.")
+    guess = int(input())
+    
+    if guess < secretNumber:
+        print("Your guess is too low, my friend.")
+    elif guess > secretNumber:
+        print("Your guess is too high, my friend.")
+    else:
+        break    # This condition is the correct guess!
+    
+if guess == secretNumber:
+    print("Haha you got it! Congrats, my friend! You guessed my number in " + str(guessesTaken) + " guesses!")
+else:
+    print("Nope. Sorry, my friend. The number I was thinking of was " + str(secretNumber) + ".")
